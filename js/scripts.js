73f72f531ecc3337a58d5e1e90d56d30e08fd3dd
Emzzy241 <emzzyoluwole@gmail.com>
2022-07-10 15:54:51 +0100

    done with the first slider: showing reviews

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+//   The swiper for the review slider 
+
+var swiper = new Swiper(".review-slider", {    
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+  });
