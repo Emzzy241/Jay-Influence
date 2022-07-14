c55ea4ad4ca8d31bf8c0698db60d2120e128f7cd
Emzzy241 <emzzyoluwole@gmail.com>
2022-07-14 08:02:31 +0100

    working with the add to cart functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,101 @@
+
+// Javascript for the add to cart 
+
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+// Open Class
+cartIcon.onclick = () =>{
+  cart.classList.add('active');
+};
+
+// Close Cart
+closeCart.onclick = () =>{
+  cart.classList.remove("active")
+};
+
+// Cart working js 
+if (document.readyState == 'loading'){
+  document.addEventListener('DOMContentLoaded', ready);
+}else {
+  ready();
+}
+
+// Making function
+
+function ready(){
+  // Remove items from cart
+  var removeCartButtons = document.getElementsByClassName('cart-remove')
+  console.log(removeCartButtons)
+  for (var i = 0; i < removeCartButtons.length; i++){
+    var button = removeCartButtons[i];
+    button.addEventListener('click', removeCartItem);
+  }
+  // Quantity Changes
+  var quantityInputs = document.getElementsByClassName('cart-quantity')
+  for (var i = 0; i < quantityInputs.length; i++){
+    var input = quantityInputs[i]
+    input.addEventListener('change', quantityChanged)
+  }
+  // Add to Cart
+  var addCart = document.getElementsByClassName("add-cart");
+  for (var i = 0; i < addCart.length; i++){
+    var button = addCart[i];
+    button.addEventListener("click", addCartClicked);
+
+  }
+
+}
+
+// Remove items from cart
+
+function removeCartItem(event){
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updateTotal();
+}
+// Quantity Changes
+function quantityChanged(event){
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateTotal();
+}
+// Add to cart
+function addCartClicked(event){
+  var button = event.target;
+  var shopProducts = button.parentElement;
+  var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+  var price = shopProducts.getElementsByClassName("price")[0].innerText;
+  var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+  console.log(title, price, productImg);
+}
+
+// Update Total
+function updateTotal(){
+  var cartContent = document.getElementsByClassName('cart-content')[0];
+  var cartBoxes = cartContent.getElementsByClassName("cart-box");
+  var total = 0;
+  for (var i = 0; i < cartBoxes.length; i++){
+    var cartBox = cartBoxes[i];
+      var priceElement = cartBox.getElementsByClassName("cart-price")[0];
+      var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+      var price = parseFloat(priceElement.innerText.replace("₦", ""));
+      var quantity = quantityElement.value;
+      total = total + price * quantity;
+      //  If price contain some kobo or cent value
+      total = Math.round(total * 100)  / 100;
+
+
+      document.getElementsByClassName('total-price')[0].innerText = "₦" + total;
+
+    
+  }
+}
+
+
 //   The swiper for the review slider 
 
 var swiper = new Swiper(".review-slider", {    
@@ -21,5 +119,3 @@ var swiper = new Swiper(".review-slider", {
     },
   });
 
-
-// Javascript for the add to cart 
